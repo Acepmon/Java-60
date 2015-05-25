8c43177407fca3c35fd44a395fc2994e3b34e436
spqr1011 <spqr1011@yahoo.com>
2015-05-25 16:37:10 +0800

    Бодлого 64

---
+++ Bodlogo64.java
@@ -0,0 +1,43 @@
+package BieDaalt;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+public class Bodlogo64 {
+
+    public static void main(String args[]){
+        
+        int Sum=0;
+        int Urjver = 0;
+        int Yalgavar = 0;
+        String NumA;
+        String NumB;
+        int numA;
+        int numB;
+        String output = " ";
+        String output1 = " ";
+        String output2 = " ";
+        
+        NumA = JOptionPane.showInputDialog("A Тоогоо оруулна уу:");
+        numA = Integer.parseInt(NumA);
+        NumB = JOptionPane.showInputDialog("B Тоогоо оруулна уу:");
+        numB = Integer.parseInt(NumB);      
+
+            
+                Sum = numA + numB;
+                output+= Sum+" ";
+                Urjver = numA * numB;
+                output1 = Urjver+" ";
+                Yalgavar = numA / numB;
+                output2 = Yalgavar+" ";
+                
+        JTextArea outputArea = new JTextArea();
+        outputArea.setText(output);
+                
+        JOptionPane.showMessageDialog(null, output, "Нийлбэр",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, output1, "Үржвэр",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, output2, "Ялгавар",JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        
+    }
+}
