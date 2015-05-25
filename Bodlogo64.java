421d3277f093691a1427441375f77b534dd4d20e
spqr1011 <spqr1011@yahoo.com>
2015-05-25 16:45:58 +0800

    Бодлого 64

--- Bodlogo64.java
+++ Bodlogo64.java
@@ -10,6 +10,7 @@ public class Bodlogo64 {
         int Sum=0;
         int Urjver = 0;
         int Yalgavar = 0;
+        int Noogdvor = 0;
         String NumA;
         String NumB;
         int numA;
@@ -17,6 +18,7 @@ public class Bodlogo64 {
         String output = " ";
         String output1 = " ";
         String output2 = " ";
+        String output3 = " ";
         
         NumA = JOptionPane.showInputDialog("A Тоогоо оруулна уу:");
         numA = Integer.parseInt(NumA);
@@ -28,8 +30,10 @@ public class Bodlogo64 {
                 output+= Sum+" ";
                 Urjver = numA * numB;
                 output1 = Urjver+" ";
-                Yalgavar = numA / numB;
+                Yalgavar = numA - numB;
                 output2 = Yalgavar+" ";
+                Noogdvor = numA/numB;
+                output3 = Noogdvor+" ";
                 
         JTextArea outputArea = new JTextArea();
         outputArea.setText(output);
@@ -37,6 +41,7 @@ public class Bodlogo64 {
         JOptionPane.showMessageDialog(null, output, "Нийлбэр",JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, output1, "Үржвэр",JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, output2, "Ялгавар",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, output3, "Ноогдвор",JOptionPane.INFORMATION_MESSAGE);
             System.exit(0);
         
     }
