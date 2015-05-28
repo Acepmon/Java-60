918f2bd70b20cb8a8da0c76ecde69209f278bed6
monday363 <monday363@yahoo.com>
2015-05-28 15:01:35 +0800

    Bodlogo 9

--- bodlogo8.java
+++ bodlogo8.java
@@ -1,35 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bodlogo;
+package bodlogogesen;
 
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
+public class Bodlogo8 {
+        public static void main(String[] args){
 
-/**
- *
- * @author JAVA M2
- */
-public class bodlogo8 {
-    public static void main(String[] args) {
-        JTextArea hub = new JTextArea();
-        String output = "";
-        int a[] = new int [15];
-        for(int i = 0; i<=14; i++)
-        {
-            a[i] = (int)(Math.random()*100);
-            output+= ""+a[i];
-            
+int[][] a = { { 20, 556, 44, 123 }, { 123, 5479, 568, 4053 } };
+
+    for (int i = 0; i < a.length; i++) {
+        int minInRow = a[i][0];
+        int maxInRow = a[i][0];
+        for (int j = 0; j < a[i].length; j++) {
+            if (minInRow > a[i][j]) {
+                minInRow = a[i][j];
+            }
+
+            if (maxInRow < a[i][j]) {
+                maxInRow = a[i][j];
+            }
         }
-        hub.setText(output);
-        JOptionPane.showMessageDialog(null,hub," Numbers ",JOptionPane.INFORMATION_MESSAGE);
-        int max = 0;
-        for(int i = 0; i<=14; i++)
-            if(a[i]>max)max=a[i];
-        JOptionPane.showMessageDialog(null," max: " +max,"хамгийн их нь:",JOptionPane.INFORMATION_MESSAGE  );
-        System.exit(0);
-                
+        System.out.println("Мөрийн хамгийн их " + maxInRow);
+        System.out.println("Мөрийн хамгийн бага " + minInRow);
+        minInRow = a[i][0];
+        
+        System.out.println("Мөрөнд байрлах хамгийн их "+i);
+        
+        maxInRow = a[i][0];
+        System.out.println("Мөрөнд байрлах хамгийн их "+ i);
+    }
+    
+    for (int[] row : a) {
+        System.out.println("\n");
+        for (int col : row) {
+            System.out.print(col + "\n");
+        }
+    }
+        
     }
 }
