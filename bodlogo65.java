a4cfeec18cf4243ef945b1ea05eb777bbfc52c33
paranora <paranora2@gmail.com>
2015-06-02 13:20:06 +0800

    Бодлого 65 -2

--- bodlogo65.java
+++ bodlogo65.java
@@ -1,61 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package daalgawar;
-import java.util.Arrays;
 import java.util.Scanner;
- 
-public class bodlogo65 {
- 
-	Scanner scan;
-	int[] num;
-	int n;
-	
-	void getVal() {
-		
-		scan = new Scanner(System.in);
-		
-		System.out.println("\n'A' д утга оруулнуу :");
-		n = Integer.parseInt(scan.nextLine());
-		
-		System.out.println("Тоонуудаа оруул :");
-		num = new int[n];
-		
-		for(int i=0; i<n; i++) {
-			
-			num[i] = Integer.parseInt(scan.nextLine());
-		}
-	}
-	
-	void sort() {
+public class bodlogo65 
+{
+    public static void main(String[] args) 
+    {
+        int n, m;
+        Scanner s = new Scanner(System.in);
+        System.out.print("array ын утгыг оруул:\n");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("элементүүдийн утгыг оруул:\n");
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (a[i] > a[j]) 
+                {
+                    m = a[i];
+                    a[i] = a[j];
+                    a[j] = m;
+                }
+            }
+        }
+        System.out.print("Өсөх дараалал:\n");
+        for (int i = 0; i < n - 1; i++) 
+        {
+            System.out.print(a[i] + " ");
+        }
+        System.out.print(a[n - 1]);
+              
+        System.out.print("\nБуурах дараалал дараалал:\n");
+        for (int i=n-1; i >= 0; i--) 
+        {
+            System.out.print(a[i] + " ");
+        }
 
-		Arrays.sort(num);
-		
-		System.out.println("\nӨсөх дараалалаар");
-		
-		for(int i=0; i<n; i++) {
-			
-			System.out.print(" " + num[i]);
-		}
-		
-		System.out.println("\nБуурах дараалалаар");
- 
-		for(int i=n-1; i>=0; i--) {
-			
-			System.out.print(" " + num[i]);
-		}
-	}
-}
- 
-class Main {
-	
-	public static void main(String args[]){
-		
-		bodlogo65 obj = new bodlogo65();
-		
-		obj.getVal();
-		obj.sort();
-	}
+    }
 }
